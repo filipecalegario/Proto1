64e95e70c2451907a620fe7ae7ba8a8bf464ca8a
filipe.calegario <filipe.calegario@gmail.com>
2012-08-21 16:59:21 -0300

    ...

---
+++ Proto1/P1TouchGestureRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  P1TouchGestureRecognizer.h
+//  Proto1
+//
+//  Created by Filipe Calegario on 08/08/12.
+//  Copyright (c) 2012 FCAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface P1TouchGestureRecognizer : UIGestureRecognizer
+
+@end
---
+++ Proto1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Proto1
+//
+//  Created by Filipe Calegario on 16/07/12.
+//  Copyright (c) 2012 FCAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "P1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([P1AppDelegate class]));
+    }
+}
